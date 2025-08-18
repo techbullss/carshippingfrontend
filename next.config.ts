ac72aa010a2e80fb75416ee1cdf0e312c05b24ab
Maish <bwanmaina2010@gmail.com>
2025-08-18 17:01:22 +0300

    ddd

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
     images: {
     domains: ['localhost', 'flagcdn.com','stainlessbackend-5.onrender.com','res.cloudinary.com']
   },
+  compiler: {
+    emotion: true,
+  },
 };
 
 export default nextConfig;
