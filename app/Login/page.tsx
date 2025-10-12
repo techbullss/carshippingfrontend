c2033053dd7de493d0dfbeeba3a64a7657018de4
Maish <bwanmaina2010@gmail.com>
2025-10-12 15:18:11 +0300

    done

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -74,12 +74,12 @@ const LoginPage = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                   </svg>
                 </div>
-                <h2 className="ml-4 text-2xl font-bold text-gray-900">UK2KE Shipping</h2>
+                <h2 className="ml-4 text-2xl font-bold text-gray-900">FcarShipping Shipping</h2>
               </div>
               <h2 className="mt-8 text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
               <p className="mt-2 text-sm text-gray-600">
                 Or{' '}
-                <Link href="/register" className="font-medium text-blue-700 hover:text-blue-600">
+                <Link href="/Signup" className="font-medium text-blue-700 hover:text-blue-600">
                   create a new account
                 </Link>
               </p>
