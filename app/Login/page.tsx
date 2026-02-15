d121689c506e5d86eff666732a5e2346603e3f8d
Maish <bwanmaina2010@gmail.com>
2026-02-15 20:30:21 +0300

    done

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -156,7 +156,7 @@ const LoginPage = () => {
                     <button
                       type="submit"
                       disabled={isLoading}
-                      className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-75 disabled:cursor-not-allowed transition-colors"
+                      className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-black  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-75 disabled:cursor-not-allowed transition-colors"
                     >
                       {isLoading ? (
                         <>
