0c8cda7fa207f50e69caa263bbf2364ddd73f439
Maish <bwanmaina2010@gmail.com>
2026-03-04 17:16:30 +0300

    nnn

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -90,7 +90,7 @@ export default function Header() {
         <Link href="/" className="flex items-center space-x-3 group">
   
   {/* Logo Icon (3 stacked shapes) */}
-  <div className="relative w-14 h-14">
+  <div className="relative w-400 h-100">
     <Image
       src="/log.png"
       alt="FCarShipping Logo"
