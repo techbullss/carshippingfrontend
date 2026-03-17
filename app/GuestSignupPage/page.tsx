8d0e42384ad542f54d94eb5e92ae8bc356a60840
Maish <bwanmaina2010@gmail.com>
2026-03-17 16:06:36 +0300

    ddd

--- app/GuestSignupPage/page.tsx
+++ app/GuestSignupPage/page.tsx
@@ -42,6 +42,7 @@ export default function GuestSignupPage() {
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
+          credentials: "include",
           body: JSON.stringify({ email, password }),
         }
       );
